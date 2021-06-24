c26bee40247236abaf69ec96a5dc9136e2708218
tnjensen <thomasnorupjensen@gmail.com>
2021-06-24 11:17:04 +0200

    Adjusted home page and menu functionality

--- script/script.js
+++ script/script.js
@@ -1,35 +1,48 @@
-const menuButton = document.querySelector('.fa-bars');
 const menu = document.querySelector('.navbar');
+const menuBtn = document.querySelector('.menu-btn');
 const subMenu = document.querySelectorAll('.item');
-const dropDown = document.querySelectorAll('.drop-btn');
 
-/* Open the menu with keyboard */ 
-menuButton.addEventListener('keypress', function(){
-    menu.classList.toggle('active');
+/* Open the submenu elements with click or keyboard */
+document.querySelectorAll('.has-dropdown').forEach(item =>
+{ 
+    item.addEventListener('click', event => {
+        item.classList.toggle('visible');
+    })
+    item.addEventListener('keypress', event =>{
+      item.classList.toggle('visible');
+    })
 })
+
+/* Close menu when clicking outside */
 document.onclick = function(event){
   let element = event.target.closest('.navbar');
   if(!element){
     menu.classList.remove('active');
   }
   let dropElement = event.target.closest('.item');
+  if(!event.target){
+    subMenu.classList.remove('visible');
+  }
   if(!dropElement){
     for(i = 0; i < subMenu.length; i++){
       subMenu[i].classList.remove('visible');
     }  
   }
-  if(event.target.classList.contains("fa-bars")){
+  if(event.target.classList.contains("fa-bars") || event.target.classList.contains("menu-btn") ){
     menu.classList.add('active');
   }
 }
-/* Open the menu dropdown elements */
-document.querySelectorAll('.has-dropdown').forEach(item =>
-{ 
-    item.addEventListener('click', event => {
-        item.classList.toggle('visible');
-    })
-    item.addEventListener('keypress', event =>{ 
-        item.classList.toggle('visible');
-    })
+// Ensure only one dropdown open 
+document.addEventListener('click', function (event) {
+
+	// Loop through each item
+	for (var i = 0; i < subMenu.length; i++) {
+
+		// If the item is the one clicked, skip it
+		if (subMenu[i] === event.target) continue;
+
+		// Remove the .active class
+		subMenu[i].classList.remove('visible');
+	}
 })
 
